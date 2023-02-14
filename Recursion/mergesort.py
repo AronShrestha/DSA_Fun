13a0ad658641f425cab4726d7490bab7ce407f89
AronShrestha <aronshrestha498@gmail.com>
2023-02-14 10:14:46 +0545

    merge sort with different approach

--- Recursion/mergesort.py
+++ Recursion/mergesort.py
@@ -1,4 +1,4 @@
-def merge(l,r):
+def merge(l,r)->list:
     i = 0
     j = 0
     a =[]
@@ -32,5 +32,40 @@ def mergesort(a):
         return merge(left,right)
 
 
+
+def merge2ndway(a,s,m,e)->None:
+    i = s
+    j = m
+    sorted_array = []
+    index_of_sorted_array = 0
+    while i < m and j < e:
+        if a[i]< a[j]:
+            sorted_array.append(a[i])
+            i+=1
+        else:
+            sorted_array.append(a[j])
+            j+=1
+        index_of_sorted_array+=1
+    if i<m:
+        while i < m:
+            sorted_array.append(a[i])
+            i+=1
+    else:
+        while j < e:
+            sorted_array.append(a[j])
+            j+=1
+    
+    for i in range(len(sorted_array)):
+        a[s+i] = sorted_array[i]
+
+
+def mergesort2nway(a,l,h)->None:
+    if h-l ==1 :
+        return 
+    m = l -int((l-h)/2)
+    mergesort2nway(a,l,m)
+    mergesort2nway(a,m,h)
+    merge2ndway(a,l,m,h)
 a=[1,99,19,2,100,3,66,29,30]
-print(mergesort(a))
+mergesort2nway(a,0,len(a))
+print(a)
